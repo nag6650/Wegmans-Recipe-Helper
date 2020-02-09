86483f8800d551867a13232c33034f029e526d66
axl8713-01 <axl8713@g.rit.edu>
2020-02-09 07:54:18 -0500

    Add key rotator to version control

---
+++ key_rotator.py
@@ -0,0 +1,28 @@
+"""
+file: key_rotator.py
+event: Brick Hack 6
+author: Albin Liang
+version: 0.0
+purpose: A module that rotates the subscription keys for Wegman's API.
+"""
+
+import queue
+KEYCHAIN = queue.Queue()
+
+def init_keys( *args ):
+    """
+    pass the Wegman's API Subscription Keys to this initializer
+    :param args: variable number of subscription keys
+    :return:
+    """
+    global KEYCHAIN
+    for arg in args:
+        KEYCHAIN.put(arg)
+
+
+def next_key(keychain):
+
+    front = keychain.get()
+    keychain.put(front)
+    print(front)
+    return front
