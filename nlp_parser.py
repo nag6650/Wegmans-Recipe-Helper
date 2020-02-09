ca067d4b63c2c4159f498ef4cbfbe0069a2911e5
Ethan Howes <edh5623@rit.edu>
2020-02-08 21:52:49 -0500

    NLP parser now utilizes scraper function.

--- nlp_parser.py
+++ nlp_parser.py
@@ -1,16 +1,21 @@
-# Imports the Google Cloud client library
 from google.cloud import language
 
 from google.cloud.language import enums
 from google.cloud.language import types
+e_types = enums.Entity.Type
 
-# Instantiates a client
+from scraping import scrape
+
+# Instantiates client
 client = language.LanguageServiceClient()
-
+# Set encoding type for documents
 encoding_type = enums.EncodingType.UTF8
+# Unwanted entity types
+bad_types = [e_types.NUMBER, e_types.DATE, e_types.ADDRESS, e_types.WORK_OF_ART]
 
 
 def ingredient_getter(ingredient_list):
+    entity_list = []
     document = types.Document(
         content=ingredient_list,
         type=enums.Document.Type.PLAIN_TEXT)
@@ -19,12 +24,15 @@ def ingredient_getter(ingredient_list):
         print(u"Representative name for the entity: {}".format(entity.name))
 
         print(u"Entity type: {}".format(enums.Entity.Type(entity.type).name))
+        if enums.Entity.Type(entity.type) not in bad_types:
+            entity_list.append(entity.name)
 
         print("\n")
+    return entity_list
 
 
 def main():
-    ingredient_getter(" 1/2. butter")
+    print(ingredient_getter(scrape("https://www.simplyrecipes.com/recipes/eggs_benedict/")))
 
 
 main()
