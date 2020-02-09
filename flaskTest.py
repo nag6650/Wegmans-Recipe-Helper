76a8a51e9f5f52b0e764e95a213d5f1518cbdf82
SmallNate99 <44757230+SmallNate99@users.noreply.github.com>
2020-02-08 23:08:13 -0500

    Working ont able

--- flaskTest.py
+++ flaskTest.py
@@ -15,12 +15,16 @@ ButtonPressed = 0
 @app.route('/confirm', methods = {'POST', 'GET'})
 def confirm():
     if request.method == 'POST':
+        ingList = ['apple', 'orange', 'milk']
         # ingList = get array
-        # try:
-        #     ingName = request.form['value']
-        #     ingList.remove(ingName)
-        # except Exception:
-        #     pass
+        try:
+            ingName = request.form['submit_button']
+            print("hi " + ingName)
+            ingList.remove(ingName)
+        except Exception:
+            print("failure")
+            pass
+        print(ingList)
         return render_template("confirm.html", ingList=ingList)
 
 
